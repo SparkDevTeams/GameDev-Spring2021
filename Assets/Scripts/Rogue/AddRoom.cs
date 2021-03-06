27e3e2f5a720de9c4196969273860b0f998664ff
jsantana29 <santanarijo29@gmail.com>
2021-03-06 14:13:40 -0500

    Item and boss rooms added
    Included guaranteed boss and item rooms in the level gen

--- Assets/Scripts/Rogue/AddRoom.cs
+++ Assets/Scripts/Rogue/AddRoom.cs
@@ -6,16 +6,50 @@ public class AddRoom : MonoBehaviour
 {
     private RoomTemplates templates;
 
+    public bool isDeadend;
+
+    public bool allowed;
+
+    public bool limited;
+    private int limitCount;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         templates.rooms.Add(this.gameObject);
+
+        //if (limited)
+        //{
+        //    templates.decrementLimit();
+        //}
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if(templates.getLimit() <= 0)
+        //{
+        //    setAllowed(false);
+        //}
+    }
+
+    public bool getAllowed()
+    {
+        return allowed;
+    }
+
+    public bool getIsDeadend()
+    {
+        return isDeadend;
+    }
+
+    public void setAllowed(bool allow)
+    {
+        allowed = allow;
     }
 }
