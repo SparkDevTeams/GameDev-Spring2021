a4c0048e8959d110b7918aac6fd5edc98a3f77bd
Vulcan400 <emil.lopez001@mymdc.net>
2021-04-08 21:34:29 -0400

    Game Over Implement
    HP Canvas Fix + Game Over added

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
@@ -85,6 +86,6 @@ public class HealthManager : MonoBehaviour
 
     public IEnumerator GameOver() {
         yield return new WaitForSeconds(3);
-        
+        SceneManager.LoadScene("MainMenu1");
     }
 }
