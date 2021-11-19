0e193aa95e10f97d6d3f8fc8867ca6f01f7ef4ac
jsantana29 <santanarijo29@gmail.com>
2021-11-18 21:53:34 -0500

    Added boss room and teleport system

---
+++ Assets/Scripts/Rogue/TeleportManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private bool isTeleported;
+    [SerializeField] private Transform bossRoom;
+    void Start()
+    {
+        isTeleported = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void teleported(bool isTeleport)
+    {
+        isTeleported = isTeleport;
+    }
+
+    public bool getTeleported()
+    {
+        return isTeleported;
+    }
+
+    public Transform getBossRoom()
+    {
+        return bossRoom;
+    }
+}
