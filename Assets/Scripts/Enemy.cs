305cfeea1eb57404f850b14edab8fb88ed7eb680
jiulen <wongjiulen.coding@gmail.com>
2023-07-07 15:41:01 +0800

    enemy changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,13 +14,4 @@ public class Enemy : Actor
         //Pool/destroy enemy
     }
 
-    private void InflictDamage()
-    {               //Add an animator event to trigger this method at the right time
-        Collider2D coll = Physics2D.OverlapCircle(attackTransform.position, attackRadius, playerLayer);
-
-         if(coll != null)
-             coll.gameObject.GetComponent<HealthManager>().damage(damagePower, 0.5f);        //Works best if the player has only one collider
-        
-    }
-
 }
